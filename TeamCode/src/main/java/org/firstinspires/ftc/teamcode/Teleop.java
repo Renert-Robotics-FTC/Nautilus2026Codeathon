96991cce55eaf9b7e49fb9889c75509e984ac3f0
kaylayschip <kayleyl2025@gmail.com>
2026-09-01 22:35:56 -0600

    connected to config, reversed motors, and added imu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -1,20 +1,52 @@
 package org.firstinspires.ftc.teamcode;
 //imports
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+
 @TeleOp(name = "Main Teleop")
-public class Teleop extends LinearOpMode{
+public class Teleop extends LinearOpMode {
+
+    //drive motors
+    private DcMotor frontRight;
+    private DcMotor frontLeft;
+    private DcMotor backRight;
+    private DcMotor backLeft;
+    private IMU imu;
 
 
     @Override
+
     public void runOpMode(){
-        //create subsystem objects here
+
+        //connect to config
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+
+        //reverse to make it go forwards
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+
+        //imu
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+                ));
+
+
 
         waitForStart();
 
         while (opModeIsActive()) {
-            //put loop code here
+
+
         }
 
     }
