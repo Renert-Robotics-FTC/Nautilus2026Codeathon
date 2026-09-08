8f573daa266479b309221f0fddbcb424a71dd423
anaya-dotcom <anaya25mishra@gmail.com>
2026-09-08 12:18:21 -0600

    Shooter subsystem created, contains function update to control shooter, indexer, beambreak. Intergrated into teleop file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -3,21 +3,26 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-@TeleOp(name = "Main Teleop")
-public class Teleop extends LinearOpMode{
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
+public class Teleop extends LinearOpMode {
 
     @Override
-    public void runOpMode(){
+    public void runOpMode() {
         //create subsystem objects here
-
+        boolean intake = false;
+        boolean shoot = false;
         waitForStart();
+        ShooterSubsystem shooter = new ShooterSubsystem(hardwareMap);
 
         while (opModeIsActive()) {
             //put loop code here
+            telemetry.update();
+            shooter.update(intake, shoot);
         }
 
-    }
 
-
+}
 }
