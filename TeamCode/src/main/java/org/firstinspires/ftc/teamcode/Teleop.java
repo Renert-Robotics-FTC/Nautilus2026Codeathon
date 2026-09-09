04c164302a63d0916edc1308e9eea27d7a9bdc2f
Demi Stathonikos <demistathonikos@Demis-MacBook-Air.local>
2026-09-08 20:27:04 -0600

    Add intake subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -14,11 +14,21 @@ public class Teleop extends LinearOpMode {
         //create subsystem objects here
         boolean intake = false;
         boolean shoot = false;
+
+        IntakeSubsystem intakeSubsystem = new IntakeSubsystem(hardwareMap);
+
         waitForStart();
         ShooterSubsystem shooter = new ShooterSubsystem(hardwareMap);
 
         while (opModeIsActive()) {
             //put loop code here
+            intake = gamepad1.a;
+            if (intake) {
+                intakeSubsystem.setPower(1.0);
+            } else {
+                intakeSubsystem.setPower(0);
+            }
+
             telemetry.update();
             shooter.update(intake, shoot);
         }
